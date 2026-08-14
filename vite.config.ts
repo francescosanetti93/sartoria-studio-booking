b6d83431d3ad5c39ce818bfeef9e9bf732cc5002
francescosanetti93 <francescosanetti93@gmail.com>
2026-08-14 14:33:29 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sartoria-studio-booking/',
   plugins: [react(), tailwindcss()],
 })
